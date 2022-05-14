974005872e3df305db8b4757c5e35efca4236617
SaazNeupane <saazneu789@gmail.com>
2022-05-14 11:10:06 +0545

    refactor(project): Projects Added

--- js/main.js
+++ js/main.js
@@ -5,150 +5,142 @@
  ------------------------------------ 
  ====================================*/
 
+"use strict";
 
-
-'use strict';
-
-
-$(window).on('load', function() { 
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
-
+$(window).on("load", function () {
+  /*------------------
+		 Preloder
+	 --------------------*/
+  $(".loader").fadeOut();
+  $("#preloder").delay(400).fadeOut("slow");
 });
 
+(function ($) {
+  /*------------------
+		 Background set
+	 --------------------*/
+  $(".set-bg").each(function () {
+    var bg = $(this).data("setbg");
+    $(this).css("background-image", "url(" + bg + ")");
+  });
 
-(function($) {
+  $(".refrence-slider").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    items: 1,
+    autoplay: true,
+  });
 
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
+  $(".progress-bar-style").each(function () {
+    var progress = $(this).data("progress");
+    var prog_width = progress + "%";
+    if (progress <= 100) {
+      $(this).append(
+        '<div class="bar-inner" style="width:' +
+          prog_width +
+          '"><span>' +
+          prog_width +
+          "</span></div>"
+      );
+    } else {
+      $(this).append(
+        '<div class="bar-inner" style="width:100%"><span>' +
+          prog_width +
+          "</span></div>"
+      );
+    }
+  });
 
+  $(".lan-prog").each(function () {
+    var progress = $(this).data("lanprogesss");
+    var ele = "<span></span>";
+    var ele_fade = '<span class="fade-ele"></span>';
 
-	$('.refrence-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 1,
-		autoplay: true
-	});
+    for (var i = 1; i <= 5; i++) {
+      if (i <= progress) {
+        $(this).append(ele);
+      } else {
+        $(this).append(ele_fade);
+      }
+    }
+  });
 
+  /*------------------
+		 Popup
+	 --------------------*/
+  $(".project-item .port-pic").magnificPopup({
+    type: "image",
+    mainClass: "img-popup-warp",
+    removalDelay: 500,
+  });
 
+  if ($().circleProgress) {
+    //Set progress circle 1
+    $("#progress1").circleProgress({
+      value: 0.65,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
+    //Set progress circle 2
+    $("#progress2").circleProgress({
+      value: 0.75,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
+    //Set progress circle white
+    $("#progress3").circleProgress({
+      value: 0.7,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
+    //Set progress circle white
+    $("#progress4").circleProgress({
+      value: 0.5,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
-	$('.lan-prog').each(function() {
-		var progress = $(this).data("lanprogesss");
-		var ele      = '<span></span>';
-		var ele_fade = '<span class="fade-ele"></span>';
-		
-		for (var i = 1; i <= 5; i++) {
-			if(i <= progress){
-				$(this).append(ele);
-			} else {
-				$(this).append(ele_fade);
-			}
-		}
-	});
+    $("#progress5").circleProgress({
+      value: 0.25,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
+    $("#progress6").circleProgress({
+      value: 0.81,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
-	/*------------------
-		Popup
-	--------------------*/
-	$('.project-item .port-pic').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 500,
-	});
+    $("#progress7").circleProgress({
+      value: 0.31,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
 
-
-
-
-if($().circleProgress){
-
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.40,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.45,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress3").circleProgress({
-		value: 0.43,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress4").circleProgress({
-		value: 0.31,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	$("#progress5").circleProgress({
-		value: 0.25,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	$("#progress6").circleProgress({
-		value: 0.81,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	$("#progress7").circleProgress({
-		value: 0.31,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	$("#progress8").circleProgress({
-		value: 0.50,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
-
+    $("#progress8").circleProgress({
+      value: 0.7,
+      size: 175,
+      thickness: 2,
+      fill: "#40424a",
+      emptyFill: "rgba(0, 0, 0, 0)",
+    });
+  }
 })(jQuery);
-
